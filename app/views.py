76af2c38dcc0ac3e5d80b1cca1cfb6226989666c
Rishi <rishikeshpatnala@gmail.com>
2025-08-12 20:59:28 +0530

    Delete app directory

--- app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from .models import *
-from .forms import *
-from django.shortcuts import get_object_or_404
-# Create your views here.
-def home(request):
-    return render(request, 'base.html')
